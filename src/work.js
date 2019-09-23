419113ff5b791e6e305bd00783371224ff821129
hppRC <hpp.ricecake@gmail.com>
2019-09-24 05:19:36 +0900

    🍣適当にやってみた

--- src/work.js
+++ src/work.js
@@ -2,7 +2,6 @@
 import { useRef } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 import { css, jsx } from '@emotion/core';
-import { PerspectiveCamera } from 'three';
 
 const theme = css`
 	width: 100vw;
@@ -10,16 +9,28 @@ const theme = css`
 `;
 
 const Thing = () => {
+	const ref = useRef();
+
+	useFrame(() => {
+		ref.current.rotation.y += 0.01;
+	});
+
 	return (
-		<mesh>
-			<PerspectiveCamera />
+		<mesh ref={ref}>
+			<sphereGeometry attach='geometry' args={[300, 30, 30]} />
+			<meshStandardMaterial attach='material' color='#FF0000' />
 		</mesh>
 	);
 };
 
 export const Work = () => (
 	<div css={theme}>
-		<Canvas>
+		<Canvas camera={{ position: [0, 0, 1000] }}>
+			<directionalLight
+				color='#FFFFFF'
+				intensity={1}
+				position={[1, 1, 1]}
+			/>
 			<Thing />
 		</Canvas>
 	</div>
